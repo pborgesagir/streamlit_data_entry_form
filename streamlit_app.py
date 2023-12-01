dbbbb5acf9341bed9652332ead2fffe0fd929f3f
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2023-12-01 19:42:54 -0300

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+
+
+
+
+st.title("Envio de Cadasdro da AGIR para fornecedores")
+st.markdown("Insira abaixo os dados para envio:")
+
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+
+
+
+existing_data = conn.read(worksheet="fornecedores", usecols = list(range(6)), ttl=5)
+existing_data = existing_data.dropna(how="all")
+
+st.dataframe(existing_data)
