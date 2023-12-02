aa2ffce6898304dc38809804c070c4f79ffc4291
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2023-12-02 13:23:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,6 +71,6 @@ with st.form(key="vendor_form"):
             updated_df = pd.concat([existing_data, vendor_data], ignore_index=True)
 
             # Update Google Sheets with the new vendor data
-            conn.update(worksheet="Vendors", data=updated_df)
+            conn.update(worksheet="fornecedores", data=updated_df)
 
             st.success("Vendor details successfully submitted!")
