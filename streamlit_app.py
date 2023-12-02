9efb80ac622ab891bc253c70416bd44042269e69
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2023-12-02 13:16:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,10 @@ from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
 
-
-
 st.title("Envio de Cadasdro da AGIR para fornecedores")
 st.markdown("Insira abaixo os dados para envio:")
 
-
 conn = st.connection("gsheets", type=GSheetsConnection)
-
-
 
 
 existing_data = conn.read(worksheet="fornecedores", usecols = list(range(6)), ttl=5)
