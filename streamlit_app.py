c9059da5d37dbf3de6335361a71f7840bf837227
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2023-12-04 05:15:49 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
 
-st.title("Envio de Cadastro da AGIR para fornecedores")
+st.title("Envio de Cadastro da AGIR para fornecedores 📤")
 st.markdown("Insira abaixo os dados para envio:")
 
 conn = st.connection("gsheets", type=GSheetsConnection)
